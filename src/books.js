d68c8be8c5e786a1f36fc8c3a7acf4f120031bf5
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-05-23 14:46:30 +0700

    base api bookshelf

---
+++ src/books.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        "id": "N1vIE1aILsMPoSg1",
+        "name": "Lord of The Rings",
+        "year": 2021,
+        "author": "John Lenon",
+        "summary": "A book about greed",
+        "publisher": "Gramedia",
+        "pageCount": 400,
+        "readPage": 4,
+        "finished": false,
+        "reading": true,
+        "insertedAt": "2023-05-23T06:30:34.686Z",
+        "updatedAt": "2023-05-23T06:30:34.686Z"
+    },
+    {
+        "id": "asdaHdsajd12aj0a",
+        "name": "Harry Potter",
+        "year": 2010,
+        "author": "Rowan Atkinson",
+        "summary": "Hogwarts Legacy",
+        "publisher": "Gramedia",
+        "pageCount": 120,
+        "readPage": 120,
+        "finished": true,
+        "reading": false,
+        "insertedAt": "2023-05-23T06:35:34.686Z",
+        "updatedAt": "2023-05-23T06:35:34.686Z"
+    }
+];
+
+module.exports = books;
